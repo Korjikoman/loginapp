03e131b8033d6208498d5feb65cf3806ea60bf00
Stanislav <showmasniok@gmail.com>
2024-10-09 19:18:35 +0700

    Added some bootstrap ^^

--- app/forms.py
+++ app/forms.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(label='username', min_length=5, max_length=15)
-    email = forms.EmailField(label='email')
+    email = forms.EmailField(label='email', widget=forms.EmailInput)
     password1 = forms.CharField( label='password', widget=forms.PasswordInput)
     password2 = forms.CharField( label='Confirm password', widget=forms.PasswordInput)
     
@@ -27,9 +27,12 @@ class RegisterForm(UserCreationForm):
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
         
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['email'].label= ''
+        
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
-        print(email)
         new = User.objects.filter(email = email)
         if new.count():
             raise ValidationError("Email already exists!!!")
@@ -41,6 +44,7 @@ class RegisterForm(UserCreationForm):
         print(password1, password2)
         if password1 and password2 and password2 != password1:
             raise ValidationError("Password don't match!!!")
+            
         return password2
     
     def save(self, commit=True):
